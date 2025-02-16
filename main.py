914da0d1d9fe5b3df1776e6a4602c6858a6ee053
Luis Cid <alexcid2002@gmail.com>
2025-02-16 23:41:46 +0000

    V1.1
    Final layout implemented with grid system

--- main.py
+++ main.py
@@ -8,11 +8,34 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
-canvas = Canvas(width=200, height=200)
-logo_image = PhotoImage(file="logo.png")
-canvas.create_image(100, 100, image=logo_image)
-canvas.pack()
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
+
+# Labels
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0, sticky="w")
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0, sticky="w")
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0, sticky="w")
+
+# Entries
+website_entry = Entry(width=50)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=50)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(width=25)
+password_entry.grid(row=3, column=1)
+
+# Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=50)
+add_button.grid(row=4, column=1, columnspan=2)
+
 
 window.mainloop()
