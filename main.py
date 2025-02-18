a07b7278896466f2ec09e44ee408b38840a822ab
Luis Cid <alexcid2002@gmail.com>
2025-02-18 13:10:17 +0000

    V1.2
    Save the user input data to a .txt file

--- main.py
+++ main.py
@@ -4,7 +4,19 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
+def save():
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}\n")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
 # ---------------------------- UI SETUP ------------------------------- #
+
 
 window = Tk()
 window.title("Password Manager")
@@ -26,15 +38,17 @@ password_label.grid(row=3, column=0, sticky="w")
 # Entries
 website_entry = Entry(width=50)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=50)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "tempmail@gmail.com")
 password_entry = Entry(width=25)
 password_entry.grid(row=3, column=1)
 
 # Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=50)
+add_button = Button(text="Add", width=42, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 
