eb2ce1affc8c330cffb8147875e42f6352d3b642
Luis Cid <alexcid2002@gmail.com>
2025-03-12 19:59:37 +0000

    V1.7
    Searching inside dictionary while handling exceptions . User can now search if he has a password for a specific website. If he has it ill be showned. Othersise an error will pop up

--- main.py
+++ main.py
@@ -59,6 +59,24 @@ def save():
             password_entry.delete(0, END)
 
 
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json", "r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File found!")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message="Website Not Found!")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -73,26 +91,28 @@ canvas.grid(row=0, column=1)
 
 # Labels
 website_label = Label(text="Website:")
-website_label.grid(row=1, column=0, sticky="w")
+website_label.grid(row=1, column=0, sticky="w", pady=5)
 email_label = Label(text="Email/Username:")
-email_label.grid(row=2, column=0, sticky="w")
+email_label.grid(row=2, column=0, sticky="w", pady=5)
 password_label = Label(text="Password:")
-password_label.grid(row=3, column=0, sticky="w")
+password_label.grid(row=3, column=0, sticky="w", pady=5)
 
 # Entries
-website_entry = Entry(width=50)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=40)
+website_entry.grid(row=1, column=1, sticky="w", pady=5)
 website_entry.focus()
-email_entry = Entry(width=50)
-email_entry.grid(row=2, column=1, columnspan=2)
+email_entry = Entry(width=60)
+email_entry.grid(row=2, column=1, columnspan=2, sticky="w", pady=5)
 email_entry.insert(0, "tempmail@gmail.com")
-password_entry = Entry(width=25)
-password_entry.grid(row=3, column=1)
+password_entry = Entry(width=36)
+password_entry.grid(row=3, column=1, sticky="w", pady=5)
 
 # Buttons
+search_password_button = Button(text="Search", width=10, command=find_password)
+search_password_button.grid(row=1, column=2, pady=5)
 generate_password_button = Button(text="Generate Password", command=generate_password)
-generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=42, command=save)
-add_button.grid(row=4, column=1, columnspan=2)
+generate_password_button.grid(row=3, column=2, pady=5)
+add_button = Button(text="Add", width=50, command=save)
+add_button.grid(row=4, column=1, columnspan=3, pady=5)
 
 window.mainloop()
